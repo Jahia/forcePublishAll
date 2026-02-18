464c10e3f3b8fe6e83495079db994c0e7b3edbc7
Florent BOURASSE <fbourasse@jahia.com>
2026-02-18 09:44:02 +0100

    Addition of a permission check at the mutation level
    Signed-off-by: Florent BOURASSE <fbourasse@jahia.com>

--- src/main/java/org/jahia/support/forcepublishall/graphql/ForcePublication.java
+++ src/main/java/org/jahia/support/forcepublishall/graphql/ForcePublication.java
@@ -9,6 +9,7 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Set;
+import javax.jcr.AccessDeniedException;
 import javax.jcr.RepositoryException;
 import org.jahia.api.Constants;
 import org.jahia.exceptions.JahiaRuntimeException;
@@ -31,6 +32,7 @@ public final class ForcePublication {
 
     private GqlJcrNodeMutation nodeMutation;
     private static final Logger logger = LoggerFactory.getLogger(ForcePublication.class);
+    private static final String PERMISSION_PUBLISH = "publish";
 
     protected void validateNodeWorkspace(GqlJcrNode node) {
         try {
@@ -65,42 +67,45 @@ public final class ForcePublication {
     public Boolean forcePublish() throws RepositoryException {
         final ComplexPublicationService complexPublicationService = BundleUtils.getOsgiService(ComplexPublicationService.class, null);
         final SchedulerService schedulerService = BundleUtils.getOsgiService(SchedulerService.class, null);
+        final JCRNodeWrapper nodeToPublish = nodeMutation.getNode().getNode();
+        if (nodeToPublish.hasPermission(PERMISSION_PUBLISH)) {
+            try {
+                final String uuid = nodeToPublish.getIdentifier();
+                final String path = nodeToPublish.getPath();
+                final Set<String> activeLiveLanguagesSet = nodeToPublish.getResolveSite().getActiveLiveLanguages();
+                final JCRSessionWrapper session = JCRSessionFactory.getInstance().getCurrentUserSession();
 
-        try {
-            final JCRNodeWrapper nodeToPublish = nodeMutation.getNode().getNode();
-            final String uuid = nodeToPublish.getIdentifier();
-            final String path = nodeToPublish.getPath();
-            final Set<String> activeLiveLanguagesSet = nodeToPublish.getResolveSite().getActiveLiveLanguages();
-            final JCRSessionWrapper session = JCRSessionFactory.getInstance().getCurrentUserSession();
+                logger.info("Force publication of node with UUID: {}, path {}", uuid, path);
+                final JobDetail jobDetail = BackgroundJob.createJahiaJob("Publication", PublicationJob.class);
+                final JobDataMap jobDataMap = jobDetail.getJobDataMap();
+                JCRTemplate.getInstance().doExecuteWithSystemSessionAsUser(null, Constants.LIVE_WORKSPACE, null, sessionWrapper -> {
+                    final JCRNodeWrapper node = sessionWrapper.getNodeByUUID(uuid);
+                    if (node == null) {
+                        throw new JahiaRuntimeException("Node not found");
+                    }
+                    node.remove();
+                    sessionWrapper.save();
+                    logger.info("Deleted node with UUID: {}, path {} in live workspace", uuid, path);
+                    return null;
+                });
+                final Collection<ComplexPublicationService.FullPublicationInfo> fullPublicationInfos = complexPublicationService.getFullPublicationInfos(Collections.singletonList(uuid), activeLiveLanguagesSet, true, session);
+                final List<String> allUuids = getAllUuids(fullPublicationInfos);
+                jobDataMap.put(PublicationJob.PUBLICATION_UUIDS, allUuids);
+                jobDataMap.put(PublicationJob.PUBLICATION_PATHS, Collections.singletonList(path));
+                jobDataMap.put(PublicationJob.SOURCE, Constants.EDIT_WORKSPACE);
+                jobDataMap.put(PublicationJob.DESTINATION, Constants.LIVE_WORKSPACE);
+                jobDataMap.put(PublicationJob.CHECK_PERMISSIONS, true);
 
-            logger.info("Force publication of node with UUID: {}, path {}", uuid, path);
-            final JobDetail jobDetail = BackgroundJob.createJahiaJob("Publication", PublicationJob.class);
-            final JobDataMap jobDataMap = jobDetail.getJobDataMap();
-            JCRTemplate.getInstance().doExecuteWithSystemSessionAsUser(null, Constants.LIVE_WORKSPACE, null, sessionWrapper -> {
-                final JCRNodeWrapper node = sessionWrapper.getNodeByUUID(uuid);
-                if (node == null) {
-                    throw new JahiaRuntimeException("Node not found");
-                }
-                node.remove();
-                sessionWrapper.save();
-                logger.info("Deleted node with UUID: {}, path {} in live workspace", uuid, path);
-                return null;
-            });
-            final Collection<ComplexPublicationService.FullPublicationInfo> fullPublicationInfos = complexPublicationService.getFullPublicationInfos(Collections.singletonList(uuid), activeLiveLanguagesSet, true, session);
-            final List<String> allUuids = getAllUuids(fullPublicationInfos);
-            jobDataMap.put(PublicationJob.PUBLICATION_UUIDS, allUuids);
-            jobDataMap.put(PublicationJob.PUBLICATION_PATHS, Collections.singletonList(path));
-            jobDataMap.put(PublicationJob.SOURCE, Constants.EDIT_WORKSPACE);
-            jobDataMap.put(PublicationJob.DESTINATION, Constants.LIVE_WORKSPACE);
-            jobDataMap.put(PublicationJob.CHECK_PERMISSIONS, true);
+                logger.info("Scheduling publication job for node with UUID: {}, path {}, will publish {} nodes in {} languages", uuid, path, allUuids.size(), activeLiveLanguagesSet.size());
+                schedulerService.scheduleJobNow(jobDetail);
+            } catch (RepositoryException | SchedulerException e) {
+                throw new JahiaRuntimeException(e);
+            }
 
-            logger.info("Scheduling publication job for node with UUID: {}, path {}, will publish {} nodes in {} languages", uuid, path, allUuids.size(), activeLiveLanguagesSet.size());
-            schedulerService.scheduleJobNow(jobDetail);
-        } catch (RepositoryException | SchedulerException e) {
-            throw new JahiaRuntimeException(e);
+            return true;
+        } else {
+            throw new AccessDeniedException(PERMISSION_PUBLISH);
         }
-
-        return true;
     }
 
     private static List<String> getAllUuids(Collection<ComplexPublicationService.FullPublicationInfo> fullPublicationInfo) {
